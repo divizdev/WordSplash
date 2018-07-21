7f01222004c337aa88b95565dccba3501b01f1d5
Meliphant <op.personal.analytics@gmail.com>
2018-07-21 14:18:15 +0300

    info screen made design

--- app/src/main/java/com/worksplash/akscorp/wordsplash/AboutActivity.java
+++ app/src/main/java/com/worksplash/akscorp/wordsplash/AboutActivity.java
@@ -1,14 +1,31 @@
 package com.worksplash.akscorp.wordsplash;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Objects;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.about_tv_fork:
+                    String urlFork = "https://github.com/vovaksenov99/WordSplash";
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(urlFork));
+                    startActivity(i);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +39,11 @@ public class AboutActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
         TextView forkUs = findViewById(R.id.about_tv_fork);
-        TextView lisence = findViewById(R.id.about_tv_lisence);
-        lisence.setText(R.string.activity_about_open_source);
-
+        forkUs.setLinkTextColor(getResources().getColor(R.color.colorAccent));
         TextView authors = findViewById(R.id.about_tv_authors);
         authors.setText(R.string.activity_about_authors_list);
 
+        forkUs.setOnClickListener(onClickListener);
     }
 
     @Override
