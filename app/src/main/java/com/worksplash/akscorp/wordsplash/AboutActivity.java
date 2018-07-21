9b12f88430a80902780cb0745db33f8572a65765
Meliphant <op.personal.analytics@gmail.com>
2018-07-21 13:51:07 +0300

    about screen

--- app/src/main/java/com/worksplash/akscorp/wordsplash/AboutActivity.java
+++ app/src/main/java/com/worksplash/akscorp/wordsplash/AboutActivity.java
@@ -1,11 +1,12 @@
 package com.worksplash.akscorp.wordsplash;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -14,9 +15,27 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            toolbar.setTitle(getResources().getString(R.string.activity_about_title));
+            Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        TextView forkUs = findViewById(R.id.about_tv_fork);
+        TextView lisence = findViewById(R.id.about_tv_lisence);
+        lisence.setText(R.string.activity_about_open_source);
 
+        TextView authors = findViewById(R.id.about_tv_authors);
+        authors.setText(R.string.activity_about_authors_list);
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
